c8f45f549b26187faf98d77a281b7af4b2678023
xili1342 <lucaliu217@outlook.com>
2022-10-16 15:23:49 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+namespace MadLibs
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+      This program ...
+      Author: ...
+      */
+
+
+      // Let the user know that the program is starting:
+
+
+      // Give the Mad Lib a title:
+      string title = "TITLE";
+
+      Console.WriteLine(title);
+      // Define user input and variables:
+
+
+      // The template for the story:
+
+      string story = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world.";
+
+
+      // Print the story:
+
+    }
+  }
+}
